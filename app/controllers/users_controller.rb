71189e321990b639675bd217d3c7c05dbffcb39b
tatu0321 <177841065+tatu0321@users.noreply.github.com>
2024-09-21 12:06:08 +0000

    [Update] users.show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,20 @@
 class UsersController < ApplicationController
   def index
+    @user = User.all
   end
 
   def show
+    @user = User.find(params[:id])
+    @books = @user.books
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update
+    redirect_to user_path(@user.id)
   end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,12 @@
 class Book < ApplicationRecord
   has_one_attched :image
+  belongs_to :user
+
+  def get_image
+    if image.attached?
+      image
+    else
+      'no_image.jpg'
+    end
+  end
 end
